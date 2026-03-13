ac19052bcf6d4274fb6a50d0d78ba394f7e18afa
WUYO Studio <wuyo@example.com>
2026-03-13 23:10:20 +0100

    Refactor: remove non-website files from tracking

--- read-pdf.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-let dataBuffer = fs.readFileSync('copywritingAI.pdf');
-
-pdf(dataBuffer).then(function (data) {
-    fs.writeFileSync('pdf-text.txt', data.text);
-    console.log("PDF text extracted to pdf-text.txt");
-}).catch(console.log);
